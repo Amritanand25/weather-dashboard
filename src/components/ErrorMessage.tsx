dd88da13a505b07b424eea7c4d11ff5d47188709
Amrit Anand <amrit.anand@rekord.in>
2024-11-23 11:57:00 +0530

    improve error view comp

--- src/components/ErrorMessage.tsx
+++ src/components/ErrorMessage.tsx
@@ -14,12 +14,23 @@ const messages = {
 
 export const ErrorMessage: React.FC<ErrorMessageProps> = ({ error }) => {
   return (
-    <div
-      className="flex items-center space-x-2 bg-red-500/10 border border-red-500/20 
+    <div>
+      <div className="my-8 mt-16 flex justify-center items-center">
+        <img
+          src="/error-animation.gif"
+          alt="Error"
+          width="150"
+          height="150"
+          className="w-[150px] h-[150px]"
+        />
+      </div>
+      <div
+        className="flex items-center space-x-2 bg-red-500/10 border border-red-500/20 
                     rounded-lg px-4 py-3 text-red-400"
-    >
-      <AlertCircle size={20} />
-      <span>{messages[error.type]}</span>
+      >
+        <AlertCircle size={20} />
+        <span>{messages[error.type]}</span>
+      </div>
     </div>
   );
 };
